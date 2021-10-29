df387629b9c176d71e2ed308df24c473fb6f1bfb
EmmanuelEstradaGod <20183ti079@utez.edu.mx>
2021-10-28 23:55:13 -0500

    Agregado registro con status a consultor

--- SAPS/src/main/java/mx/edu/utez/saps/controller/ConsultorController.java
+++ SAPS/src/main/java/mx/edu/utez/saps/controller/ConsultorController.java
@@ -26,6 +26,7 @@ public class ConsultorController {
 	
 	@PostMapping("/save")
 	public boolean saveConsultor(@RequestBody ConsultorEntity consultor) {
+		consultor.setStatus(true);
 		return consultorService.saveConsultor(consultor);
 	}
 	
